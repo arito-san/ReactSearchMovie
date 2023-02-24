f43ea7bce180371a2665440cf232afb146fbc727
arito-san <tie.jr9@gmail.com>
2023-02-24 01:32:19 -0300

    feat:home&card

---
+++ src/page/Index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+
+function Home() {
+    return (
+        <div className="main">
+            <div className='card'>
+                <div>image</div>
+                <div>title</div>
+                <div>released</div>
+                <div>genre</div>
+                <div>director</div>
+            </div>
+        </div>
+    )
+}
+export default Home
